26d6c756525cd9fe3543c18dea67800ff123f492
Deepak Sharma <dsharma2828@gmail.com>
2024-07-21 22:53:10 +0530

    try 1

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,31 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState } from "react";
+import "./../styles/App.css";
 
 const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+  const [name, setName] = useState("");
+  const [greeting, setGreeting] = useState("");
 
-export default App
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+  };
+
+  const displayGreeting = () => {
+    setGreeting(`Hello, ${name}! Welcome to our site.`);
+  };
+
+  return (
+      <div>
+        {/* Do not remove the main div */}
+        <input
+            type="text"
+            placeholder="Enter your name"
+            value={name}
+            onChange={handleNameChange}
+        />
+        <button onClick={displayGreeting}>Greet Me</button>
+        {greeting && <p>{greeting}</p>}
+      </div>
+  );
+};
+
+export default App;
